1dad487d666f46b0e0e65ed9a29732f254e596af
gmaldon2 <gabrielmacolon@gmail.com>
2023-03-19 15:16:01 -0400

    Finished the connection between django and - postgresql database.

--- website/models.py
+++ website/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Club(models.Model):
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     created_at =  models.DateTimeField(auto_now_add=True)
+    students = ArrayField(models.IntegerField(), null=True)
+
     def __str__(self):
         return self.name

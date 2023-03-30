a3d94438bf87a5bc1c25c6bdd81fbaa35a751959
gmaldon2 <gabrielmacolon@gmail.com>
2023-03-29 20:18:38 -0400

    Changes. Stable.

--- website/views.py
+++ website/views.py
@@ -6,9 +6,19 @@ def home(request):
     return render(request, 'index.html')
 
 def discover(request):
+    gaming_count = Club.objects.filter(category='Gaming')
+    hobbies_count = Club.objects.filter(category='Hobbies')
+    academic_count = Club.objects.filter(category='Academic')
+    sports_count = Club.objects.filter(category='Sports')
+    social_count = Club.objects.filter(category='Social')
     objects = Club.objects.all()
     return render(request, 'discover.html', {
-        'clubs' : objects
+        'clubs' : objects,
+        'social_count' : len(social_count),
+        'hobbies_count' : len(hobbies_count),
+        'academic_count' : len(academic_count),
+        'gaming_count' : len(gaming_count),
+        'sports_count' : len(sports_count),
     })
 
 def joined(request):

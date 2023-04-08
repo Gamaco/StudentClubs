99b6470cc15f7fd3a80b1763da94a0459ecebb2c
gmaldon2 <gabrielmacolon@gmail.com>
2023-04-08 17:41:13 -0400

    unstable. Sign up works. Missing Sign in.

--- website/urls.py
+++ website/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('discover/', views.discover, name='discover'),
     path('joined/', views.joined, name='joined'),
     path('club-creation/', views.club_creation, name='club-creation'),
-    path('clubinformation/<int:c_id>', views.clubinformation, name='clubinformation')
+    path('clubinformation/<int:c_id>', views.clubinformation, name='clubinformation'),
+    path('signup/', views.signup, name='signup')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
